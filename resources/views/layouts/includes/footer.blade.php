b7083cc3545468a4a8821bef83eb4d7239d73940
Yihsinyeah <samuel0928426@gmail.com>
2019-12-04 20:09:09 +0800

    homepage

---
+++ resources/views/layouts/includes/footer.blade.php
@@ -0,0 +1,21 @@
+<!-- Footer -->
+<footer class="bg-light py-5">
+  <div class="container">
+    <div class="small text-center text-muted">Copyright &copy; 2019 - Start Bootstrap</div>
+  </div>
+</footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="{{asset ('app-assets/vendor/jquery/css/jquery.min.js')}}"></script>
+<script src="{{asset ('app-assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+<!-- Plugin JavaScript -->
+<script src="{{asset ('app-assets/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+<script src="{{asset ('app-assets/vendor/magnific-popup/jquery.magnific-popup.min.js')}}"></script>
+
+<!-- Custom scripts for this template -->
+<script src="{{asset ('app-assets/vendor/js/creative.min.js')}}"></script>
+
+</body>
+
+</html>
---
+++ resources/views/layouts/includes/header.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>遺失物招領平台</title>
+
+  <!-- Font Awesome Icons -->
+  <link href = "{{asset ('app-assets/vendor/bootstrap/css/all.min.css')}}" rel="stylesheet" type="text/css">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet">
+  <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
+
+  <!-- Plugin CSS -->
+  <link href="{{asset ('app-assets/vendor/magnific-popup/magnific-popup.css')}}" rel="stylesheet">
+
+
+  <!-- Theme CSS - Includes Bootstrap -->
+  <link href="{{asset ('app-assets/css/creative.min.css')}}" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">首頁</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto my-2 my-lg-0">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">會員登入</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#services">遺失物查詢</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#portfolio">懸賞頁面</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#contact">管理者登入</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Masthead -->
+  <header class="masthead">
+    <div class="container h-100">
+      <div class="row h-100 align-items-center justify-content-center text-center">
+        <div class="col-lg-10 align-self-end">
+          <h1 class="text-uppercase text-white font-weight-bold">東西不見了嗎？來這邊找找看吧！</h1>
+          <hr class="divider my-4">
+        </div>
+        <div class="col-lg-8 align-self-baseline">
+          <p class="text-white-75 font-weight-light mb-5">使用說明打這邊</p>
+          <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find Out More</a>
+        </div>
+      </div>
+    </div>
+  </header>
